2a00a5f650a640046f31495d057d71132dc0c206
Leo-Ying314 <leoleoying@gmail.com>
2021-07-14 15:00:11 -0700

    First Assignment Version
    Initial commit

--- izhikevich_cells.py
+++ izhikevich_cells.py
@@ -45,16 +45,16 @@ import matplotlib.pyplot as plt
 
 
 class izhCell():
-    def __init__(self,stimVal):
+    def __init__(self, stimVal):
         # Define Neuron Parameters
-        self.celltype='Generic Izhikevich' # Regular spiking
+        self.celltype = 'Generic Izhikevich' # Regular spiking
         self.C=100
         self.vr=-60
         self.vt=-40
         self.k=0.7
         self.a=0.03
         self.b=-2
-        self.c=-56
+        self.c=-50
         self.d=100
         self.vpeak=35
         self.stimVal = stimVal
@@ -108,3 +108,4 @@ def createCell():
     
 if __name__=='__main__':
     createCell()
+
